c3d94e337138d6c0483b9715f2482b48449f3285
holin <holin.he@gmail.com>
2011-10-12 00:04:56 +0800

    bu

--- app/models/cheese/token.rb
+++ app/models/cheese/token.rb
@@ -3,7 +3,7 @@ module Cheese
     set_table_name "cheese_tokens"
 
     def status(content)
-      client = OauthChina.const_get(kind.camelize).load(:access_token => token.token, :access_token_secret => token.secret)
+      client = OauthChina.const_get(kind.camelize).load(:access_token => token, :access_token_secret => secret)
       client.add_status(content)
     end
     
